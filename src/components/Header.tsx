a50491aac894ef46e50eb4c68b24644ea9c3b469
Fernando Junior <fernandobfr10@gmail.com>
2024-04-02 10:28:19 -0300

    feat: :sparkles: create navlink component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "./NavLink";
+
 import logo from "../assets/logo.svg";
 
 export function Header() {
@@ -5,12 +7,8 @@ export function Header() {
     <header className="flex items-center gap-5 py-2">
       <img src={logo} alt="Logo do pass.in" />
       <nav className="flex items-center gap-5">
-        <a href="" className="font-medium text-sm text-zinc-300">
-          Eventos
-        </a>
-        <a href="" className="font-medium text-sm">
-          Participantes
-        </a>
+        <NavLink href="/eventos">Eventos</NavLink>
+        <NavLink href="/participantes">Participantes</NavLink>
       </nav>
     </header>
   );
